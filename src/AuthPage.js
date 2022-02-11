781d34fab642747026be6c8e40207a28a234b01b
Victor Caruso <vic.caruso@gmail.com>
2022-02-11 11:37:12 -0800

    set up auth form and verified signin/up working with supabase auth

--- src/AuthPage.js
+++ src/AuthPage.js
@@ -1,7 +1,39 @@
 import React from 'react';
+import { useState } from 'react';
+import { signIn, signUp } from './services/fetch-utils';
 
 export default function AuthPage({ setCurrentUser }) {
+  const [formEmail, setFormEmail] = useState('');
+  const [formPass, setFormPass] = useState('');
+
+  async function handleSignIn(e) {
+    e.preventDefault();
+
+    const user = await signIn(formEmail, formPass);
+    setCurrentUser(user);
+    setFormEmail('');
+    setFormPass('');
+  }
+
+  async function handleSignUp() {
+    const user = await signUp(formEmail, formPass);
+    setCurrentUser(user);
+    setFormEmail('');
+    setFormPass('');
+  }
+
   return (
-    <div>AuthPage</div>
+    <div className='auth-form'>
+      <form onSubmit={handleSignIn}>
+        <label>email
+          <input type='text' value={formEmail} onChange={e => setFormEmail(e.target.value)}></input>
+        </label>
+        <label>password
+          <input type='password' value={formPass} onChange={e => setFormPass(e.target.value)}></input>
+        </label>
+        <button type='submit'>Sign In</button>
+        <button type='button' onClick={handleSignUp}>Sign Up</button>
+      </form>
+    </div>
   );
 }
