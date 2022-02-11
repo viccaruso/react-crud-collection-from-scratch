8fc34590826dab8f987c2d05fb70631dda310516
Victor Caruso <vic.caruso@gmail.com>
2022-02-11 11:21:52 -0800

    set up routes to check auth state and redirect as needed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import AuthPage from './AuthPage';
+import { BrowserRouter, Switch, Route, Redirect, Link, NavLink } from 'react-router-dom';
 import { useState } from 'react';
+import AuthPage from './AuthPage';
 
 function App() {
   const [currentUser, setCurrentUser] = useState(localStorage.getItem('supabase.auth.token'));
-  
+
   return (
-    <div className="App">
-      <AuthPage setCurrentUser={setCurrentUser} />
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/'>
+          {
+            currentUser
+              ? <Redirect to='/restaurants' />
+              : <Redirect to='/authenticate' />
+          }
+        </Route>
+        <Route exact path='/authenticate' >
+          {
+            currentUser
+              ? <Redirect to='/restaurants' />
+              : <AuthPage setCurrentUser={setCurrentUser} />
+          }
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
